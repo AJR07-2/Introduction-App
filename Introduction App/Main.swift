4271cd6b96ae70ec865303cead53172a77f7766a
Ang Jun Ray <angjunray@live.com>
2021-06-23 10:52:05 +0800

    FInished!

--- Introduction App/Main.swift
+++ Introduction App/Main.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct Main: View {
     @State var showingAlert = false
+    @State var showingDescription = false
+    @State var descOpacity = 0.1
     var body: some View {
         VStack{
             HStack{
@@ -62,16 +64,46 @@ struct Main: View {
                     .alert(isPresented: $showingAlert) {
                          Alert(title: Text("That link a bit sketchy right?"), message: Text("XD"), dismissButton: .default(Text("Yeah nice rickroll")))
                      }
+                    
+                    Button(action: {
+                        descOpacity = 0.1
+                    }, label: {
+                        Link(destination: URL(string: "https://dunjudge.me/users/ajr/")!, label: {
+                            Text("Dunjudge")
+                        })
+                    })
                 }
                 .frame(width: 400, height: 70, alignment: .center)
                 .background(Color.yellow)
                 .cornerRadius(1000.0)
             }
-            Button(action: {
-                
-            }, label:{
-                Text("Hello I do not know what to write here")
-            })
+            
+            if(showingDescription){
+                VStack{
+                    Text("Wow you found this nice")
+                        .multilineTextAlignment(.leading)
+                    Text("I guess it wasn't actually rlly that hidden...")
+                        .multilineTextAlignment(.leading)
+                    Text("Ah well i might as well just show my descriptions")
+                        .multilineTextAlignment(.leading)
+                    Text("My main hobby is essentially just coding...")
+                        .multilineTextAlignment(.leading)
+                    Text("HA JK I can't think of anything HA u  found this for absolutely.........NOTHING")
+                        .multilineTextAlignment(.leading)
+                }
+                .opacity(descOpacity)
+                .onTapGesture {
+                    withAnimation {
+                        descOpacity += 0.1
+                    }
+                }
+            } else {
+                Button(action: {
+                    showingDescription = true
+                }, label:{
+                    Text("Hello I do not know what to write here")
+                })
+            }
         }
     }
 }
